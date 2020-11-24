424b91005e4ceb4746529c26045d57109dfeaa21
Alex Liu <dsa1314@gmail.com>
2020-11-24 23:21:17 +0800

    feat: 更新 vue-final-modal plugin 初始化

--- src/plugin/vue-final-modal/index.ts
+++ src/plugin/vue-final-modal/index.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import VueFinalModal from 'vue-final-modal'
-Vue.use(VueFinalModal)
+import VueFinalModal, { VfmOptions } from 'vue-final-modal'
+
+Vue.use<VfmOptions>(VueFinalModal(), {
+  componentName: 'VueFinalModal',
+  key: 'vfm'
+})
